a57ad539968958c2e375c9fee1e6ef76296c9280
akos500 <akos500@yandex.ru>
2022-09-18 21:41:47 +0300

    Комменарии

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, принимающая на вход три числа и выдающая максимальное.
+
+Console.WriteLine("Введите три числа : ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ((a > b) && (a >c))
+    {
+        return a;
+    }
+
+    else if ((a < b) && (b > c))
+    {
+        return b;
+    }
+
+    else
+    {
+        return c;
+    }
